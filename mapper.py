c37f83271a135aa467bc051a5114cfedfc5c4905
sonali111-tech <75426438+sonali111-tech@users.noreply.github.com>
2021-09-06 20:52:53 +0530

    mapper.py

---
+++ mapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# import numpy as np
+import sys
+import json
+import datetime
+import re
+
+for line in sys.stdin:
+  try:
+    record = json.loads(line)
+    record_is_clean = is_clean(record)
+    if record_is_clean:
+      first_task(record,sys.argv[1])
+    else:
+      continue
+  except Exception as e:
+    continue    
+    
+def first_task(record,word):
+	
+	
+	
+	
+	
